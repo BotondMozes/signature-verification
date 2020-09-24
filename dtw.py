03f5a66107bd9d1262c6f8ce392f64c6ffeb12e4
Mozes Botond <mozesbotond2000@gmail.com>
2020-09-24 13:23:53 +0300

    fast dtw with multiprocessing

--- dtw.py
+++ dtw.py
@@ -1,7 +1,13 @@
 import os
+import time
+
 import pandas as pd
 import numpy as np
+import multiprocessing as mp
 import matplotlib.pyplot as plt
+
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -33,7 +39,29 @@ def dtw(filename1, filename2, dimension):
                 [dtw_matrix[i-1, j], dtw_matrix[i, j-1], dtw_matrix[i-1, j-1]])
             dtw_matrix[i, j] = cost + last_min
 
-    return dtw_matrix[n, m]/(n+m)
+    file1 = filename1.split("/")
+    file1 = file1[len(file1)-1]
+
+    file2 = filename2.split("/")
+    file2 = file2[len(file2)-1]
+
+    return file1 + "\t" + file2 + "\t" + str(dtw_matrix[n, m]/(n+m))
+
+
+def fast_dtw(filename1, filename2):
+    df1 = pd.read_csv(filename1)
+    df2 = pd.read_csv(filename2)
+
+    n, m = len(df1[df1.columns[0]]), len(df2[df2.columns[0]])
+
+    scaler = MinMaxScaler()
+
+    df1 = scaler.fit_transform(df1)
+    df2 = scaler.fit_transform(df2)
+
+    distance, path = fastdtw(df1, df2, dist=euclidean)
+
+    return distance/(n+m)
 
 
 def plot_signature(filename):
@@ -45,18 +73,36 @@ def plot_signature(filename):
 
 
 def main():
+    start_seconds = time.time()
+
     PATH = "/home/mozesbotond/WorkSpace/Signature Verification/data/MCYT/"
     FOLDER = "0000/"
 
     signature_list = os.listdir(PATH+FOLDER)
+    signature_list.sort()
 
-    file1 = signature_list[0]
-    file2 = signature_list[1]
+    print("Number of processors:", mp.cpu_count())
+
+    # file1 = signature_list[0]
+    # file2 = signature_list[1]
+
+    # print("dtw: ", dtw(PATH+FOLDER+file1, PATH+FOLDER+file2, 5))
+    # print("fast dtw: ", fast_dtw(PATH+FOLDER+file1, PATH+FOLDER+file2))
+
+    pool = mp.Pool(mp.cpu_count())
 
     for file1 in signature_list:
         for file2 in signature_list:
-            print(file1 + "\t" + file2 + "\t" +
-                  str(dtw(PATH+FOLDER+file1, PATH+FOLDER+file2, 5)))
+            if file1 != file2:
+                # result = [pool.apply(dtw, args=(
+                #     PATH+FOLDER+file1, PATH+FOLDER+file2, 5))]
+                print(file1 + " \t " + file2 + " \t " + str(pool.apply(fast_dtw, args=(
+                    PATH+FOLDER+file1, PATH+FOLDER+file2))))
+
+    pool.close()
+
+    end_seconds = time.time()
+    print("Elapsed time: ", end_seconds-start_seconds)
 
 
 main()
