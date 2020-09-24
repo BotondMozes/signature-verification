3af98cc2caa97485a2c05fb42d5290ec1ac897c6
Mozes Botond <mozesbotond2000@gmail.com>
2020-09-24 10:12:35 +0300

    normalization

--- dtw.py
+++ dtw.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 
 def dtw(filename1, filename2, dimension):
@@ -9,6 +10,11 @@ def dtw(filename1, filename2, dimension):
     df2 = pd.read_csv(filename2)
 
     n, m = len(df1[df1.columns[0]]), len(df2[df2.columns[0]])
+
+    scaler = MinMaxScaler()
+
+    df1 = scaler.fit_transform(df1)
+    df2 = scaler.fit_transform(df2)
 
     dtw_matrix = np.zeros((n+1, m+1))
     for i in range(n+1):
@@ -20,8 +26,7 @@ def dtw(filename1, filename2, dimension):
         for j in range(1, m+1):
             sum = 0
             for k in range(0, dimension):
-                index = df1.columns[k]
-                sum += (df1[index][i-1] - df2[index][j-1]) ** 2
+                sum += (df1[i-1][k] - df2[j-1][k]) ** 2
 
             cost = np.sqrt(sum)
             last_min = np.min(
@@ -40,10 +45,13 @@ def plot_signature(filename):
 
 
 def main():
-    PATH = "./WorkSpace/Signature Verification/data/MCYT/"
+    PATH = "/home/mozesbotond/WorkSpace/Signature Verification/data/MCYT/"
     FOLDER = "0000/"
 
     signature_list = os.listdir(PATH+FOLDER)
+
+    file1 = signature_list[0]
+    file2 = signature_list[1]
 
     for file1 in signature_list:
         for file2 in signature_list:
